034e0a4779b86e444d4885ec9a34656b83482610
Gabriel Sykes <48891219+sykesgabri@users.noreply.github.com>
2024-08-08 07:12:05 +0100

    Add files via upload

--- scripts/js.js
+++ scripts/js.js
@@ -1,11 +1,5 @@
-// Note to anyone looking at this, I suck at JS.
+// Note, there is currently nothing here.
+// You can pull off a lot without JS.
 
-// Check User Agent string to see if the user is on a mobile browser, and redirect them to the mobile optimised site if they are.
-
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    // true for mobile device
-    location.replace("https://m.gabriel-sykes.com")
-}else{
-    // false for not mobile device
-    void(null)
-}
+// There is the Bootstrap JS, linked in all of the site's HTML documents.
+// Bootstrap isn't the be all and end all of this site, but I do use it to simplify development.
